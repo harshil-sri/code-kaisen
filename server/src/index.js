0d63e9964929ef39a2c3c78d539c47d52ee8f130
Harshil <thisisharshil@gmail.com>
2026-03-19 19:14:33 +0000

    Migrated to WSL2 and added backend Domain HP logic

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors()); 
 const server = http.createServer(app);
+const activeDomains = {};
 
 // calling socket.io and defining cors again
 const io = new Server(server, {
@@ -19,11 +20,18 @@ io.on('connection', (socket) => {
     socket.on('join_domain', (roomName) => {
         socket.join(roomName);
         console.log(`User ${socket.id} entered domain: ${roomName}`);
+
+        if (!activeDomains[roomName]) {
+            activeDomains[roomName] = { player1HP: 100, player2HP: 100};
+        }
+        io.to(roomName).emit('health_update', activeDomains[roomName]);
     });
     socket.on('client_attack', (roomName) => {
-        console.log(`💥 Attack fired in domain: ${roomName}`);
-        
-        io.to(roomName).emit('server_damage', "Attack landed! HP -10"); 
+        if (activeDomains[roomName]) {
+            activeDomains[roomName].player2HP -=10;
+            console.log(`💥 Attack fired in domain: ${roomName}. P2 HP: ${activeDomains[roomName].player2HP}`);
+        }
+        io.to(roomName).emit('health_update', activeDomains[roomName]);
     });
 });
 const PORT = 3001;
